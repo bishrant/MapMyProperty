ceb03c70355dbfef3c35d3767947f9a001e3edd8
Bishrant Adhikari <bishrant.adhikari@tfs.tamu.edu>
2020-04-30 16:33:45 -0500

    Separated heading on to its own module

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit {
+  @Input('headerColor') headerColor: String | undefined;
+  @Input('title') title: String = '';
+  public headerStyle: any = {};
+  constructor() {}
+
+  ngOnInit(): void {
+    this.headerStyle = { 'background-color': this.headerColor };
+  }
+}
