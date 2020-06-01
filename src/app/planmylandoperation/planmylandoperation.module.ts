4f84208de969f599dcecc100ae63392bae3ea2c6
Fernando Vara <fvara@tfs.tamu.edu>
2020-06-01 11:41:42 -0500

    PMLO initial state

--- src/app/planmylandoperation/planmylandoperation.module.ts
+++ src/app/planmylandoperation/planmylandoperation.module.ts
@@ -4,6 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PmloComponent } from './pmlo/pmlo.component';
 import { HeaderModule } from '../shared/components/header/header.module';
+import { EsrimapComponent } from './esrimap/esrimap.component';
+import { DrawtoolsModule } from '../shared/modules/drawtools/drawtools.module';
+import { SidebarModule } from '../mapmyproperty/sidebar/sidebar.module';
+import { GraphicsStoreModule } from '../shared/store/GraphicsStore.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 const routes: Routes = [
   {
@@ -12,11 +20,20 @@ const routes: Routes = [
   },
 ];
 @NgModule({
-  declarations: [PmloComponent],
+  declarations: [
+    PmloComponent,
+    EsrimapComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    HeaderModule
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+    FlexLayoutModule,
+    GraphicsStoreModule,
+    HeaderModule,
+    DrawtoolsModule,
+    SidebarModule,
   ]
 })
 export class PlanmylandoperationModule { }
