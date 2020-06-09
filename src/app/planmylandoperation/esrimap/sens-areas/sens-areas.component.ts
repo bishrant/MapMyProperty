f4caedd8cbf858ee06a7d693f7cb98b06812858d
Fernando Vara <fvara@tfs.tamu.edu>
2020-06-09 16:53:38 -0500

    added functionality for closing tool when warnings

--- src/app/planmylandoperation/esrimap/sens-areas/sens-areas.component.ts
+++ src/app/planmylandoperation/esrimap/sens-areas/sens-areas.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { CreateSensAreasGL } from '../../pmloUtils/layers';
 import GraphicsLayer from 'arcgis-js-api/layers/GraphicsLayer';
 import { DialogService } from 'src/app/shared/components/dialogs/dialog.service';
 import { GreaterThanMaxArea } from 'src/app/shared/utils/GeometryEngine';
 import { DecimalPipe } from '@angular/common';
+import { MatExpansionPanel } from '@angular/material/expansion';
 
 @Component({
   selector: 'pmlo-sens-areas',
@@ -11,6 +12,8 @@ import { DecimalPipe } from '@angular/common';
   styleUrls: ['./sens-areas.component.scss']
 })
 export class SensAreasComponent implements OnInit {
+
+  @ViewChild ('sensAreaToolHeader') sensAreaToolHeader: MatExpansionPanel;
 
   state: string = "noBoundary";
   streamCollapsed:  boolean = true;
@@ -45,16 +48,17 @@ export class SensAreasComponent implements OnInit {
     };
     const maxAcres:number = 10000;
 
-    debugger
-
     if (this.boundaryLayer.graphics.length === 0) {
       this.state = 'noBoundary';
     } else if (this.boundaryLayer.graphics.length > 1) {
+      this.sensAreaToolHeader.close();
       opt.message = 'You can only display sensitive areas from one polygon at a time.';
       this.dialogService.open(opt);
     } else if (GreaterThanMaxArea(this.boundaryLayer.graphics.items[0].geometry, maxAcres, "acres")) {
+      this.sensAreaToolHeader.close();
       opt.message = 'Please make sure the boundary is less than ' + this.decimalPipe.transform(maxAcres) + ' acres';
       this.dialogService.open(opt);
+
     } else if (this.sensAreaGL.graphics.length === 0) {
       this.state = 'clipping';
     } else {
