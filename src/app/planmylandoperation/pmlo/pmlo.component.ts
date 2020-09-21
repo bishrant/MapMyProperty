879c416aa312486f80c19558844b5f2ae2070132
Fernando Vara <fvara@tfs.tamu.edu>
2020-09-21 15:16:54 -0500

    removed empty scss

--- src/app/planmylandoperation/pmlo/pmlo.component.ts
+++ src/app/planmylandoperation/pmlo/pmlo.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pmlo',
-  templateUrl: './pmlo.component.html',
-  styleUrls: ['./pmlo.component.scss']
+  templateUrl: './pmlo.component.html'
 })
 export class PmloComponent implements OnInit {
   constructor () { }
