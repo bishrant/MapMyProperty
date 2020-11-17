69c7889827beef06809ac0023df20efd70f91f77
bishrant <adhi_bishrant@hotmail.com>
2020-11-17 16:03:27 -0600

    Created restore session functionality for PMLO

--- src/app/shared/store/storage.metareducer.ts
+++ src/app/shared/store/storage.metareducer.ts
@@ -1,34 +1,16 @@
-import { ActionReducer, MetaReducer } from '@ngrx/store';
-import { Action} from '@ngrx/store';
-
-function setSavedState(state: any, localStorageKey: string) {
+function setSavedState(state: any) {
   localStorage.setItem(localStorageKey, JSON.stringify(state));
 }
+
+function clearLocalStorage() {
+  localStorage.removeItem(localStorageKey);
+}
+
 function getSavedState(): any {
   return JSON.parse(localStorage.getItem(localStorageKey));
 }
-//keys from state which we'd like to save
-const stateKeys = ['graphics'];
+
 // the key for the local storage
 const localStorageKey = '__mmp_storage__';
 
-function storage(reducer: ActionReducer<any>): ActionReducer<any>{
-  let onInit = true;
-  return function (state, action) {
-    let nextState = reducer(state, action);
-
-    if (onInit) {
-      onInit = false;
-    } else {
-      // nextState = state;
-      console.log(state);
-      setSavedState(nextState, localStorageKey);
-    }
-    // need to filter
-    return reducer(nextState, action);
-  }
-}
-
-const metaReducers: MetaReducer<any>[] = [storage];
-
-export {metaReducers, getSavedState}
+export {getSavedState, clearLocalStorage, setSavedState}
