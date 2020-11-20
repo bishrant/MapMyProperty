7cb36c0a3a7b12388ac574be472463117ec404ab
Fernando Vara <fvara@tfs.tamu.edu>
2020-11-20 13:59:00 -0600

    added fontawesome module

--- src/app/shared/components/overlay-layers-widget/overlay-layers-widget.module.ts
+++ src/app/shared/components/overlay-layers-widget/overlay-layers-widget.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverlayLayersWidgetComponent } from './overlay-layers-widget.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [OverlayLayersWidgetComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   exports: [OverlayLayersWidgetComponent]
 })
