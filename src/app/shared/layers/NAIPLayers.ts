7337bfd4dc90463ab685ffdf199bddc8761b473e
bishrant <adhi_bishrant@hotmail.com>
2020-11-24 16:45:52 -0600

    Fixes some of Brad Issues with draw tools #196

--- src/app/shared/layers/NAIPLayers.ts
+++ src/app/shared/layers/NAIPLayers.ts
@@ -10,32 +10,32 @@ const googleWMSlayer = new WMSLayer({
 
 const topo1996 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP96_CIR_1m/ImageServer", id: 'topo1996' });
 const naip2004 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP04_CIR_1m/ImageServer", id: 'TX NAIP 2004' });
-const topo2008 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP08_NC_50cm/ImageServer", id: 'TX TOPO 2008' });
-const topo2009 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP09_NC_50cm/ImageServer", id: 'TX TOPO 2009' });
+const topo2008 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP08_NC_50cm/ImageServer", id: 'TX TOP 2008' });
+const topo2009 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP09_NC_50cm/ImageServer", id: 'TX TOP 2009' });
 const naip2010 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP10_NC_CIR_1m/ImageServer", id: 'TX NAIP 2010' });
 const naip2012 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP12_NC_CIR_1m/ImageServer", id: 'TX NAIP 2012' });
 const naip2014 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP14_NC_CIR_1m/ImageServer", id: 'TX NAIP 2014' });
-const topo2015 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP15_NC_CIR_50cm/ImageServer", id: 'TX TOPO 2015' });
+const topo2015 = new ImageryLayer({ url: _tnrisURL + "TOP/TOP15_NC_CIR_50cm/ImageServer", id: 'TX TOP 2015' });
 const naip2016 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP16_NC_CIR_1m/ImageServer", id: 'TX NAIP 2016' });
 const naip2018 = new ImageryLayer({ url: _tnrisURL + "NAIP/NAIP18_NC_CIR_60cm/ImageServer", id: 'TX NAIP 2018' });
 
 
-const texasBasemaps = ["TX Google 6-inch", "TX NAIP 2018", "TX NAIP 2016", "TX TOPO 2015", "TX NAIP 2014",
+const texasBasemaps = ["TX Google 6-inch", "TX NAIP 2018", "TX NAIP 2016", "TX TOP 2015", "TX NAIP 2014",
   "TX NAIP 2012", "TX NAIP 2010",
-  "TXe TOPO 2009", "TXw TOPO 2008", "TX NAIP 2004", "TX TOPO 1996"];
+  "TXe TOP 2009", "TXw TOP 2008", "TX NAIP 2004", "TX TOP 1996"];
 
 const texasBasemapsDict = {
   "TX Google 6-inch": googleWMSlayer,
   "TX NAIP 2018": naip2018,
   "TX NAIP 2016": naip2016,
-  "TX TOPO 2015": topo2015,
+  "TX TOP 2015": topo2015,
   "TX NAIP 2014": naip2014,
   "TX NAIP 2012": naip2012,
   "TX NAIP 2010": naip2010,
-  "TXe TOPO 2009": topo2009,
-  "TXw TOPO 2008": topo2008,
+  "TXe TOP 2009": topo2009,
+  "TXw TOP 2008": topo2008,
   "TX NAIP 2004": naip2004,
-  "TX TOPO 1996": topo1996,
+  "TX TOP 1996": topo1996,
 }
 
 
