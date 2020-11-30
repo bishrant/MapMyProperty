e47fc0592aec3c51b60e3d72faead3379501e2c6
Fernando Vara <fvara@tfs.tamu.edu>
2020-11-30 16:40:50 -0600

    Updated draw help

--- src/app/shared/components/help-components/draw-help/draw-help.module.ts
+++ src/app/shared/components/help-components/draw-help/draw-help.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DrawHelpComponent } from './draw-help.component';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSvgIconModule } from 'angular-svg-icon';
 
 
 
 @NgModule({
   declarations: [DrawHelpComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule
   ],
   exports: [DrawHelpComponent]
 })
