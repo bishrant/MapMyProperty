d18819393244ae62205206a08108cde2c3bfddb5
bishrant <adhi_bishrant@hotmail.com>
2020-12-08 14:35:07 -0600

    Updated draw tools to reflect real time changes from siderbar

--- src/app/planmylandoperation/sidebar/soils/soils.component.ts
+++ src/app/planmylandoperation/sidebar/soils/soils.component.ts
@@ -220,8 +220,8 @@ export class SoilsComponent implements OnInit {
     this.soilsService.clearSoilGLayers(this.pmloSoilsGL, this.pmloSoilLabelsGL);
   }
 
-  updateSliderValue (value: number):void {
-    this.soilsService.updateSliderValue.emit({ sliderVal: value, isFromSoils: true, selectedRadioVal: null });
+  updateSliderValue (value: any):void {
+    this.soilsService.updateSliderValue.emit({ sliderVal: parseInt(value), isFromSoils: true, selectedRadioVal: null });
   }
 
   toggleTable ():void {
@@ -288,6 +288,7 @@ export class SoilsComponent implements OnInit {
   }
 
   private checkIfOrange (val:number):void {
+    console.log(val < 75 || val === 100, val);
     if (val < 75 || val === 100) {
       this.isOrangeSymbol = false;
     } else {
