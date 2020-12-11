095a6f52c9bd127bf2bf96b3c9a9b3f27955bbc3
Fernando Vara <fvara@tfs.tamu.edu>
2020-12-11 08:51:24 -0600

    added measurement help

---
+++ src/app/shared/components/help-components/measurement-widget-help/measurement-widget-help.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement-widget-help',
+  templateUrl: './measurement-widget-help.component.html',
+  styleUrls: ['./measurement-widget-help.component.scss']
+})
+export class MeasurementWidgetHelpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
