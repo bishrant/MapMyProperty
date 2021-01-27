8bee32c30cf5686f19bf46bbbad5f212596e059c
Bishrant Adhikariu <bishrant.adhikari@tfs.tamu.edu>
2021-01-27 09:41:55 -0600

    Added popup for soils in MMP themes

---
+++ src/app/mapmyproperty/models/mmpSoil.model.ts
@@ -0,0 +1,22 @@
+export class MMPSoil {
+  constructor (
+        public mukey: string = '',
+        public musym: string = '',
+        public muname: string = '',
+        public slopegradwta: string = '',
+        public forpehrtdcp: string = '',
+        public flodfreqdcd: string = '',
+        public drclassdcd: string = '',
+        public hydclprs: string = '',
+        public pH: string = '',
+        public ecoclassid: string = '',
+        public ecoclassname: string = '',
+        public si: string = '',
+        public plantcomname: string = '',
+        public fpa: string = '',
+        public rpa: string = '',
+        public saversion: string = '',
+        public tabularversion: string = '',
+        public spatialversion: string = ''
+  ) { }
+}
--- src/app/planmylandoperation/pmloUtils/popupContent.ts
+++ src/app/planmylandoperation/pmloUtils/popupContent.ts
@@ -1,9 +1,10 @@
+import { MMPSoil } from 'src/app/mapmyproperty/models/mmpSoil.model';
 import { PMLOSoil } from '../models/pmloSoil.model';
 
 const GetPMLOSoilPopupContent = (soil: PMLOSoil) => {
   const ecoclassidUrl:string = 'https://edit.jornada.nmsu.edu/catalogs/esd/' + soil.ecoclassid.substring(1, 5) + '/' + soil.ecoclassid;
 
-    const popupContent: string = `
+  const popupContent: string = `
       <div class="pmlo-popup-content">
         <p class="mt-2"><b>Map Unit Name</b></p>
         <p>${soil.muname}</p>
@@ -15,46 +16,44 @@ const GetPMLOSoilPopupContent = (soil: PMLOSoil) => {
         <p>${soil.comppct_r}</p>
 
         <p class="mt-2"><b>Taxonomic Classification</b></p>
-        <p>${soil.taxclname !== '' ? soil.taxclname : '&mdash;' }</p>
+        <p>${soil.taxclname !== '' ? soil.taxclname : '&mdash;'}</p>
 
         <p class="mt-2"><b>A Horizon</b></p>
-        <p>${soil.Horizon_A !== '' ? soil.Horizon_A : '&mdash;' }</p>
+        <p>${soil.Horizon_A !== '' ? soil.Horizon_A : '&mdash;'}</p>
 
         <p class="mt-2"><b>B Horizon</b></p>
-        <p>${soil.Horizon_B !== '' ? soil.Horizon_B : '&mdash;' }</p>
+        <p>${soil.Horizon_B !== '' ? soil.Horizon_B : '&mdash;'}</p>
 
-        <p class="mt-2"><b>Slope Gradient (%)</b></p>
-        <p>${soil.slopegradwta !== '' ? soil.slopegradwta : '&mdash;' }</p>
 
         <p class="mt-2"><b>Erosion Class</b></p>
-        <p>${soil.erocl !== '' ? soil.erocl : '&mdash;' }</p>
+        <p>${soil.erocl !== '' ? soil.erocl : '&mdash;'}</p>
 
         <p class="mt-2"><b>Drainage Class</b></p>
-        <p>${soil.drclassdcd !== '' ? soil.drclassdcd : '&mdash;' }</p>
+        <p>${soil.drclassdcd !== '' ? soil.drclassdcd : '&mdash;'}</p>
 
         <p class="mt-2"><b>Depth to Water Table (inches)</b></p>
-        <p>${soil.wtdepannmin !== '' ? soil.wtdepannmin : '&mdash;' }</p>
+        <p>${soil.wtdepannmin !== '' ? soil.wtdepannmin : '&mdash;'}</p>
 
         <p class="mt-2"><b>Hydric Rating</b></p>
         <p>${soil.Hydric_Rating}</p>
 
         <p class="mt-2"><b>Flooding Frequency</b></p>
-        <p>${soil.flodfreqdcd !== '' ? soil.flodfreqdcd : '&mdash;' }</p>
+        <p>${soil.flodfreqdcd !== '' ? soil.flodfreqdcd : '&mdash;'}</p>
 
         <p class="mt-2"><b>Presence of Ponding</b></p>
         <p>${soil.pondfreqprs}</p>
-        
+
         <p class="mt-2"><b>Site Index Max Value</b></p>
-        <p>${soil.si !== '' ? soil.si : '&mdash;' }</p>
+        <p>${soil.si !== '' ? soil.si : '&mdash;'}</p>
 
         <p class="mt-2"><b>Site Index Max Value Species</b></p>
-        <p>${soil.plantcomname !== '' ? soil.plantcomname : '&mdash;' }</p>
+        <p>${soil.plantcomname !== '' ? soil.plantcomname : '&mdash;'}</p>
 
         <p class="mt-2"><b>Ecological Class ID</b></p>
-        <p>${soil.ecoclassid !== '' ? '<a href="' + ecoclassidUrl + '" target="_blank" rel="noopener noreferrer">' + soil.ecoclassid + '</a>' : '&mdash;' }</p>
+        <p>${soil.ecoclassid !== '' ? '<a href="' + ecoclassidUrl + '" target="_blank" rel="noopener noreferrer">' + soil.ecoclassid + '</a>' : '&mdash;'}</p>
 
         <p class="mt-2"><b>Ecological Class</b></p>
-        <p>${soil.ecoclassname !== '' ? soil.ecoclassname : '&mdash;' }</p>
+        <p>${soil.ecoclassname !== '' ? soil.ecoclassname : '&mdash;'}</p>
 
         <p class="mt-2"><b>Const. Limit. - Haul Rds & Log Landings</b></p>
         <p>${soil.CLRoadLndg}</p>
@@ -108,4 +107,64 @@ const GetPMLOSoilPopupContent = (soil: PMLOSoil) => {
   return popupContent;
 }
 
-export { GetPMLOSoilPopupContent }
+const GetMMPSoilPopupContent = (soil: MMPSoil) => {
+  const ecoclassidUrl:string = 'https://edit.jornada.nmsu.edu/catalogs/esd/' + soil.ecoclassid.substring(1, 5) + '/' + soil.ecoclassid;
+
+  const popupContent: string = `
+      <div class="pmlo-popup-content">
+        <p class="mt-2"><b>Map Unit Symbol</b></p>
+        <p>${soil.musym}</p>
+
+        <p class="mt-2"><b>Map Unit Name</b></p>
+        <p>${soil.muname}</p>
+
+        <p class="mt-2"><b>Slope Gradient (%)</b></p>
+        <p>${soil.slopegradwta !== '' ? soil.slopegradwta : '&mdash;'}</p>
+
+        <p class="mt-2"><b>Potential Erosion Hazard</b></p>
+        <p>${soil.forpehrtdcp}</p>
+
+        <p class="mt-2"><b>Flooding Frequency</b></p>
+        <p>${soil.flodfreqdcd}</p>
+
+        <p class="mt-2"><b>Drainage Class</b></p>
+        <p>${soil.drclassdcd}</p>
+
+        <p class="mt-2"><b>Hydric Rating (%)</b></p>
+        <p>${soil.hydclprs}</p>
+
+        <p class="mt-2"><b>pH Top Horizon:</b></p>
+        <p>${soil.pH}</p>
+
+        <p class="mt-2"><b>Ecological Class ID</b></p>
+        <p>${soil.ecoclassid !== '' ? '<a href="' + ecoclassidUrl + '" target="_blank" rel="noopener noreferrer">' + soil.ecoclassid + '</a>' : '&mdash;'}</p>
+
+        <p class="mt-2"><b>Ecological Class</b></p>
+        <p>${soil.ecoclassname !== '' ? soil.ecoclassname : '&mdash;'}</p>
+
+        <p class="mt-2"><b>Range Productivity (lb/ac/yr)</b></p>
+        <p>${soil.rpa}</p>
+
+        <p class="mt-2"><b>Forest productivity (cuft/ac/yr)</b></p>
+        <p>${soil.fpa}</p>
+
+        <p class="mt-2"><b>Site Index Max Value</b></p>
+        <p>${soil.si !== '' ? soil.si : '&mdash;'}</p>
+
+        <p class="mt-2"><b>Site Index Max Value Species</b></p>
+        <p>${soil.plantcomname !== '' ? soil.plantcomname : '&mdash;'}</p>
+
+        <p class="mt-2"><b>Survey Area Version</b></p>
+        <p>${soil.saversion}</p>
+
+        <p class="mt-2"><b>Tabular Version</b></p>
+        <p>${soil.tabularversion}</p>
+
+        <p class="mt-2"><b>Spatial Version</b></p>
+        <p>${soil.spatialversion}</p>
+      </div>
+    `;
+  return popupContent;
+}
+
+export { GetPMLOSoilPopupContent, GetMMPSoilPopupContent }
