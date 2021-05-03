793c2120e0694bc740adfb15222b0fd854f3bf1d
Bishrant Adhikariu <bishrant.adhikari@tfs.tamu.edu>
2021-05-03 16:26:05 -0500

    Removed arcgis-webpack plugin

--- src/app/shared/utils/FeatureCollectionUtils.ts
+++ src/app/shared/utils/FeatureCollectionUtils.ts
@@ -4,7 +4,7 @@ import Graphic from '@arcgis/core/Graphic';
 import Polyline from '@arcgis/core/geometry/Polyline';
 import Polygon from '@arcgis/core/geometry/Polygon';
 import { id } from '../store/todo';
-import webMercatorUtils = require('@arcgis/core/geometry/support/webMercatorUtils');
+import * as webMercatorUtils from '@arcgis/core/geometry/support/webMercatorUtils';
 
 const createPolygonGraphic = (coordinates: any) => {
   const polygonAttributes: any = {
